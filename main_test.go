a48b2c9d2536b61f0ee2d67e01adb04812083666
Khaled Farah <mrkhaled02@gmail.com>
2020-11-13 19:08:56 +0400

    Testing the login endpoint

--- main_test.go
+++ main_test.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"goseed/models/service"
 	"goseed/routers"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -15,6 +16,29 @@ func initServer() *httptest.Server {
 	router := routers.InitRoute()
 	ts := httptest.NewServer(router)
 	return ts
+}
+
+func sanityTests(t *testing.T, resp *http.Response, err error) {
+	// Make a request to our server with the {base url}/ping
+	if err != nil {
+		t.Fatalf("Expected no error, got %v", err)
+	}
+
+	if resp.StatusCode != 200 {
+		t.Fatalf("Expected status code 200, got %v", resp.StatusCode)
+	}
+
+	val, ok := resp.Header["Content-Type"]
+
+	// Assert that the "content-type" header is actually set
+	if !ok {
+		t.Fatalf("Expected Content-Type header to be set")
+	}
+
+	// Assert that it was set as expected
+	if val[0] != "application/json; charset=utf-8" {
+		t.Fatalf("Expected \"application/json; charset=utf-8\", got %s", val[0])
+	}
 }
 
 func createUser(URL string) (*http.Response, error) {
@@ -38,27 +62,7 @@ func TestSignupRoute(t *testing.T) {
 	// The setupServer method, that we previously refactored
 	// is injected into a test server
 	resp, err := createUser(ts.URL)
-
-	// Make a request to our server with the {base url}/ping
-	if err != nil {
-		t.Fatalf("Expected no error, got %v", err)
-	}
-
-	if resp.StatusCode != 200 {
-		t.Fatalf("Expected status code 200, got %v", resp.StatusCode)
-	}
-
-	val, ok := resp.Header["Content-Type"]
-
-	// Assert that the "content-type" header is actually set
-	if !ok {
-		t.Fatalf("Expected Content-Type header to be set")
-	}
-
-	// Assert that it was set as expected
-	if val[0] != "application/json; charset=utf-8" {
-		t.Fatalf("Expected \"application/json; charset=utf-8\", got %s", val[0])
-	}
+	sanityTests(t, resp, err)
 	userservice := service.Userservice{}
 	defer userservice.Delete("abc@xyz.com")
 }
@@ -73,25 +77,29 @@ func TestLoginRoute(t *testing.T) {
 
 	// Let's create a user first
 	userCreationResp, _ := createUser(ts.URL)
+	sanityTests(t, userCreationResp, nil)
 	userservice := service.Userservice{}
 	defer userservice.Delete("abc@xyz.com")
-	if userCreationResp.StatusCode != 200 {
-		t.Fatalf("Expected status code 200, got %v", userCreationResp.StatusCode)
-	}
+
 	// makes a POST request to signup
 	postBody := map[string]interface{}{
 		"email":    "abc@xyz.com",
 		"password": "test123",
+		"name":     "test",
 	}
 	body, _ := json.Marshal(postBody)
 	resp, err := http.Post(fmt.Sprintf("%s/login", ts.URL), "Application/JSON", bytes.NewReader(body))
+	// Sanity checks first
+	sanityTests(t, resp, err)
 
-	// Make a request to our server with the {base url}/ping
-	if err != nil {
-		t.Fatalf("Expected no error, got %v", err)
-	}
+	// decoder := json.NewDecoder(resp.Body)
 
-	if resp.StatusCode != 200 {
-		t.Fatalf("Expected status code 200, got %v", resp.StatusCode)
+	resBody, _ := ioutil.ReadAll(resp.Body)
+	var tokenHolder map[string]interface{}
+
+	json.Unmarshal([]byte(resBody), &tokenHolder)
+
+	if tokenHolder["token"] == "" {
+		t.Fatalf("Expected token , got %s", tokenHolder["token"])
 	}
 }
